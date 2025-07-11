205e21d6a733c1ad2502c8db03c79ff8dc54ef50
Cristian <cherreraleiz@gmail.com>
2025-07-11 17:32:07 -0400

    - update: model dto gratification added

--- app/dtos/input_models/mexico_gratification_input.rb
+++ app/dtos/input_models/mexico_gratification_input.rb
@@ -7,7 +7,7 @@ module InputModels
         attribute :daily_salary, :decimal
 
         validates :worked_days_year,  numericality: { greater_than: 0, message: "El número de días trabajados en el año no pueden ser menor o igual a 0" }
-        validates :worked_days_year,  numericality: { less_than: 365, message: "El número de días trabajados en el año no pueden ser mayor a 365" }
+        validates :worked_days_year,  numericality: { less_than: 366, message: "El número de días trabajados en el año no pueden ser mayor a 365" }
         validates :daily_salary,   numericality: { greater_than: 0, message: "El salario diario no puede ser menor a cero" }
 
     end
