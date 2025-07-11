fd728ae6651767186872906b6316e21d0d17e266
Cristian <cherreraleiz@gmail.com>
2025-07-11 17:39:42 -0400

    - update: last code modification

--- app/dtos/input_models/chile_gratification_input.rb
+++ app/dtos/input_models/chile_gratification_input.rb
@@ -7,7 +7,7 @@ module InputModels
         attribute :minimum_monthly_income, :decimal
 
         validates :monthly_base_salary,  numericality: { greater_than: 0, message: "El sueldo base mensual no puede ser menor a cero" }
-        validates :minimum_monthly_income,   numericality: { greater_than: 0, message: "El ingreso mínimo mensual no puede ser menor a cero" }
+        validates :minimum_monthly_income,   numericality: { greater_than_or_equal_to: 0, message: "El ingreso mínimo mensual no puede ser menor a cero" }
     end
 
 end
